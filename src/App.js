1e85b68170da8dc3160fbb09e88b4960850145b4
byun <markorea9708@gmail.com>
2023-09-25 23:44:37 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
-
+import docker_logo from '/src/public/docker_logo'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='page-container dark:bg-slate-800'>
+      <div className='header text-2xl shadow-xl border-solid border-4 border-indigo-200 border-x-indigo-500'>
+        Sentiment Analysis API
+        <br></br>
+        <br></br>
+        <div className='text-sm'>created using
+          <img src={docker_logo} alt="docker"></img>
+        </div>
+      </div>
+      <div className='result-container'>
+          <textarea className='user-textarea rounded-md shadow-xl border-solid border-4 border-indigo-500/100'></textarea>
+          <div className='api-response rounded-full shadow-xl border-solid border-4 border-indigo-500/100'>the response</div>
+      </div>
     </div>
-  );
+
+    );
 }
 
 export default App;
